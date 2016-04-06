55d6c2e57cd344a365a2e8f2f164aa77acd8d221
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2016-04-06 18:04:50 +0200

    enviar postal, seleccion de imagenes

--- web/js/grayscale.js
+++ web/js/grayscale.js
@@ -6,6 +6,7 @@
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
+    $("select").imagepicker();
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -17,8 +18,8 @@ $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -28,17 +29,17 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
+$('.navbar-collapse ul li a').click(function () {
+    if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+        $('.navbar-toggle:visible').click();
+    }
 });
 
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
+google.maps.event.addDomListener(window, 'resize', function () {
     map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
 });
 
